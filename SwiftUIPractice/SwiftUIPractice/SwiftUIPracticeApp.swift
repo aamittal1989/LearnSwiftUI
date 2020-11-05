26616ae8e4f5ceddf86d75d814348f70423dd8f2
Aatish Mittal <aamittal@microsoft.com>
2020-11-05 14:12:09 +0530

    first commit

---
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by AATISH MITTAL on 10/4/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
